92a0cc3ce4aea589882f0619dd3a5caf2bba0953
Alejandro Pascuale <alejandropascuale@MBPdeAlejandro.fibertel.com.ar>
2021-06-09 18:59:13 -0300

    archivo app.js para levantar servidor local

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const publicFolder = path.resolve(__dirname, './public');
+app.use(express.static(publicFolder));
+
+app.listen(8000, () => {
+    console.log('Servidor Funcionando en el puerto 3000');
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/home.html'))
+});
