9014426e8321fb66657464449f30bd1d532088aa
alejandropascuale <apascuale@outlook.com>
2021-07-01 11:41:22 -0300

    agrego ruta para producto y controlador

--- controllers/productController.js
+++ controllers/productController.js
@@ -5,20 +5,8 @@ const controller = {
     index: (req,res) => {
         return res.render ('index');
     },
-    registro: (req, res) => {
-        return res.render ('register-user');
-    },
-    registroRestaurante: (req, res) => {
-        return res.render ('register-restaurant');
-    },
-    loginUser: (req, res) => {
-        return res.render ('login');
-    },
-    loginNegocio: (req, res) => {
-        return res.render ('loginNegocio');
-    },
-    carrito: (req, res) => {
-        return res.render ('carrito');
+    detail: (req,res) => {
+        return res.render ('product')
     }
 }
 
