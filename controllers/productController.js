6319d2653f01de298636278177a85ce98af5e1da
alejandropascuale <apascuale@outlook.com>
2021-07-14 16:22:42 -0300

    vista de gestion de platos con ruta y controlador hecho

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,5 +1,16 @@
 const express = require('express');
-const router = express.Router ();
+const path = require('path');
+const router = express.Router();
+const fs = require('fs');
+const { fileLoader } = require('ejs');
+const { FILE } = require('dns');
+
+const restaurantFilePath = './data bases/restaurantDataFile.json';
+const restaurantDataBase = JSON.parse(fs.readFileSync(restaurantFilePath, 'utf-8'));
+const ordersFilePath = './data bases/ordersDataFile.json';
+const ordersDataBase = JSON.parse(fs.readFileSync(ordersFilePath, 'utf-8'));
+const productsFilePath = './data bases/productsDataFile.json';
+const productsDataBase = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
 const controller = {
     index: (req,res) => {
@@ -7,6 +18,11 @@ const controller = {
     },
     detail: (req,res) => {
         return res.render ('product')
+    },
+    productsList: (req, res) => {
+        const restaurantSelect = restaurantDataBase.find(r => r.idRestaurant == req.params.idRestaurant);
+        const productsRestaurant = productsDataBase.filter(p => p.idRestaurant == req.params.idRestaurant);
+        return res.render ('buisness-products-list', {productsRestaurant, restaurantSelect, productsDataBase})
     }
 }
 
