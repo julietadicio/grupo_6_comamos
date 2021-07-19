dc0be606ffda12a6dbf224be68f7bf0310a1a723
alejandropascuale <apascuale@outlook.com>
2021-07-19 13:45:03 -0300

    renombro vista carrito y elimino archivo test

--- test.js
+++
@@ -1,8 +0,0 @@
-const bcrypt = require('bcryptjs');
-
-const password = 'test2021'
-const passwordVillaCrespo = 'hola123'
-
-const newpassword = bcrypt.hashSync(passwordVillaCrespo, 10);
-
-console.log(newpassword);
