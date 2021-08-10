01cadb281984e5141d23f3a56d8b0469ed7cf6fb
alejandropascuale <apascuale@outlook.com>
2021-08-10 16:10:28 -0300

    configuracion de sequelize y modelo de usuario hecho

--- database/models/User.js
+++ database/models/User.js
@@ -1,30 +1,33 @@
 
-const User = sequelize.define('Users', cols, config);
+module.exports = function (sequelize, DataTypes) {
+    let alias = 'Users';
 
-const cols = {
-    id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true
+    let cols = {
+     id: {
+         type: DataTypes.INTEGER,
+         primaryKey: true,
+         autoincrement: true
     },
-    nombre: {
-        type: DataTypes.STRING
-    },
-    apellido: {
-        type: DataTypes.STRING
-    },
-    email: {
-        type: DataTypes.STRING
-    },
-    password: {
-        type: DataTypes.STRING
-    },
+     nombre: {
+         type: DataTypes.STRING
+     },
+     apellido: {
+         type: DataTypes.STRING
+     },
+     email: {
+         type: DataTypes.STRING
+     },
+     password: {
+         type: DataTypes.STRING
+     },
     perfil: {
         type: DataTypes.STRING
-    },
-
+    }
+    }
+    let config = {
+        tableName: 'users',
+        timestamps: false
+    }
+    let User = sequelize.define(alias, cols, config);
+    return User;
 }
-const config = {
-    tablename: 'users'
-}
-
-module.exports (User)
