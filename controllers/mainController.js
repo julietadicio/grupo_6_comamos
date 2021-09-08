70501f62de5d938bbe3472eece3a126e194d6ce7
alejandropascuale <apascuale@outlook.com>
2021-09-08 12:29:29 -0300

    sliders de home implementados

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -5,25 +5,34 @@ const db = require('../database/models');
 
 const controller = {
     index: async (req,res) => {
-        const products = await db.Product.findAll()
+        const products = await db.Product.findAll();
         const index = [];
         while(index.length < 7){
         const r = Math.floor(Math.random() * products.length);
         if(index.indexOf(r) === -1) index.push(r);
         }
         const randomProducts = index.map (e => products[e]);
+
+        const restaurants = await db.Restaurant.findAll();
+        const arrayRestaurants = [];
+        while(arrayRestaurants.length < 3){
+        const r = Math.floor(Math.random() * restaurants.length);
+        if(arrayRestaurants.indexOf(r) === -1) arrayRestaurants.push(r);
+        }
+        const randomRestaurants = arrayRestaurants.map (e => restaurants[e]);
+
         if (req.session.userLogged && req.session.userLogged.perfil == 'usuario') {
             const user = await db.User.findOne({
                 where: {email: req.session.userLogged.email}
             })    
-            return res.render ('index', {randomProducts, user});
+            return res.render ('index', {randomProducts, randomRestaurants, user});
         } else if (req.session.userLogged && req.session.userLogged.perfil == 'negocio') {
             const user = await db.Restaurant.findOne({
                 where: {email: req.session.userLogged.email}
             })    
-            return res.render ('index', {randomProducts, user});
+            return res.render ('index', {randomProducts, randomRestaurants, user});
         } else {
-            return res.render ('index', {randomProducts});
+            return res.render ('index', {randomProducts, randomRestaurants});
         }
     },
     listaRestaurantes: (req, res) => {
