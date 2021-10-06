0a66cd7f4a28308e1b3f4c479939acb07e5dbaf7
alejandropascuale <apascuale@outlook.com>
2021-10-05 21:05:08 -0300

    archivo shop.js para armado del carrito terminado

--- public/js/cart.js
+++ public/js/cart.js
@@ -7,19 +7,23 @@ window.addEventListener('load', async () => {
 
     let buisness = await (await fetch (`http://localhost:8000/api/buisness/${cartUser[0].id_restaurant}`)).json();
 
-    restaurantName.innerText = buisness.nombre;
-    restaurantAddress.innerText = buisness.direccion;
-
-    let a = document.createElement("a");
-    let p = document.createElement("p");
-    let article = document.createElement("article");
-    let productName = document.createTextNode(cartUser[0].plato);
-    let deleteProduct = document.createTextNode('Quitar producto');
-
-
-    p.appendChild(productName);
-    a.appendChild(deleteProduct);
-    article.appendChild(p);
-    article.appendChild(a);
-    productCartSection.appendChild(article)
+    for (let i = 0; i < cartUser.length; i++) {
+        const element = cartUser[i];
+        
+        restaurantName.innerText = buisness.nombre;
+        restaurantAddress.innerText = buisness.direccion;
+        
+        let a = document.createElement("a");
+        let p = document.createElement("p");
+        let article = document.createElement("article");
+        let productName = document.createTextNode(cartUser[0].plato);
+        let deleteProduct = document.createTextNode('Quitar producto');
+        
+        
+        p.appendChild(productName);
+        a.appendChild(deleteProduct);
+        article.appendChild(p);
+        article.appendChild(a);
+        productCartSection.appendChild(article)
+    }
 })
--- public/js/shop.js
+++ public/js/shop.js
@@ -2,41 +2,34 @@ window.addEventListener('load', async () => {
     let carts = document.querySelectorAll('.btn');
 
     let products = await (await fetch ('http://localhost:8000/api/products')).json();
-    /* let cartProducts = []; */
+    let restaurants = await (await fetch ('http://localhost:8000/api/buisness')).json();
+    let cartProducts = [];
+    if(localStorage.getItem('cartProducts')){
+        cartProducts = JSON.parse(localStorage.getItem('cartProducts'));
+    }
     
     for (let i= 0; i< carts.length; i++) {
         carts[i].addEventListener('click', () => {
             let nameProduct = carts[i].parentElement.querySelector('h3').innerText;
             let productSelect = products.filter(p => p.plato == nameProduct);
-            /* if(localStorage.getItem('cartItem')){
-                cartProducts = JSON.parse(localStorage.getItem('cartItem'));
+            
+            let buisnessKey = restaurants.find(b => b.idRestaurant == productSelect[0].id_restaurant).idRestaurant;
+            let cartByBuisness = {};
+            if(cartProducts.find(e => e.id == buisnessKey)){
+                console.log('El restaurante ya existe en el array');
+                let index = cartProducts.findIndex(x => x.id == buisnessKey);
+                cartProducts[index].products.push(productSelect[0]);
+            } else {
+                console.log('hay que agregar este negocio');
+                cartByBuisness = {
+                    id: buisnessKey,
+                    products: productSelect
+                };
+                cartProducts.push(cartByBuisness)
             }
-            console.log(productSelect);
-            cartProducts.push(productSelect) */
-            localStorage.setItem('cartItem', JSON.stringify(productSelect))
+            localStorage.setItem('cartProducts', JSON.stringify(cartProducts));
             window.location.href = 'http://localhost:8000/user/carrito';
         })     
     }
-    /* 
-    function onLoadCartNumbers () {
-        let productNumbers = Number(localStorage.getItem('cartNumbers'));
-        if (productNumbers) {
-            document.querySelector('#align-header > section > div:nth-child(1) > a > i > span').textContent = productNumbers;
-        }
-    }
-    
-    function cartNumbers () {
-        //console.log('el producto elegido es ');
-        let productNumbers = Number(localStorage.getItem('cartNumbers'));
-        if (productNumbers) {
-            localStorage.setItem('cartNumbers', productNumbers + 1);
-            document.querySelector('#align-header > section > div:nth-child(1) > a > i > span').textContent = productNumbers + 1;
-        } else {
-            localStorage.setItem('cartNumbers', 1);
-            document.querySelector('#align-header > section > div:nth-child(1) > a > i > span').textContent = 1;
-        }
-    }
-    
-    onLoadCartNumbers(); */
 })
     
