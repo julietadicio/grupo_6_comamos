93c994834c15dc77ca90994c25eaf3774f442b1d
alejandropascuale <apascuale@outlook.com>
2021-10-07 12:42:39 -0300

    agrego funcion para calcular el costo por producto a medida que aumento cantidades

--- public/js/cart.js
+++ public/js/cart.js
@@ -1,3 +1,4 @@
+
 window.addEventListener('load', async () => {
 
     let cartUser = JSON.parse(localStorage.getItem('cartProducts'));
@@ -18,61 +19,107 @@ window.addEventListener('load', async () => {
         
         
         let div = document.createElement('div');
+        div.id = 'box-restaurant'
         div.appendChild(restaurantName);
         div.appendChild(restaurantAddress);
         
         let productsByRestaurant = products.filter(y=> y.id_restaurant == (cartUser[i].id));
         for (let p = 0; p < productsByRestaurant.length; p++) {
             const product = productsByRestaurant[p];
+            let boxProduct = document.createElement('div');
+            boxProduct.id = 'box-product'
+            let input0 = document.createElement('input');
             let input1 = document.createElement('input');
             let input2 = document.createElement('input');
             let input3 = document.createElement('input');
-            let date = document.createElement('input');
+            let input4 = document.createElement('input');
+            let input5 = document.createElement('input');
             let deleteIcon = document.createElement('i');
             let link = document.createElement('a');
             let br = document.createElement('br');
 
+            
+            input0.name = 'idPlato';
+            input0.type = 'number';
+            input0.value = product.idPlato;
+            input0.style.display = 'none';
             input1.value = product.plato;
             input1.name = 'plato';
             input1.disabled = true;
             input2.value = product.precio;
             input2.name = 'precio';
+            input2.id = 'precio';
             input2.disabled = true;
+            input2.type = 'number';
             input3.type = 'number';
+            input3.value = 1;
             input3.name = 'quantity';
-            input3.placeholder = 'ingrese cantidad';
-            date.type = 'datetime-local';
-            date.name = 'date';
+            input3.id = 'quantity';
+            input4.name = 'id_restaurant';
+            input4.type = 'number';
+            input4.value = product.id_restaurant;
+            input4.style.display = 'none';
+            input5.id = 'total';
+            input5.name = 'total-product';
+            input5.type = 'number';
+            input5.value = input2.value * input3.value;
+            input5.disabled = true;
+
             deleteIcon.classList = 'fas fa-times';
             deleteIcon.id = product.idPlato;
             
-            div.appendChild(input1);
-            div.appendChild(input2);
-            div.appendChild(input3);
-            div.appendChild(date);
-            div.appendChild(link);
+            boxProduct.appendChild(input0);
+            boxProduct.appendChild(input1);
+            boxProduct.appendChild(input2);
+            boxProduct.appendChild(input3);
+            boxProduct.appendChild(input4);
+            boxProduct.appendChild(input5);
+            boxProduct.appendChild(link);
             link.appendChild(deleteIcon);
-            div.appendChild(br);
+            boxProduct.appendChild(br);
+            div.appendChild(boxProduct)
 
-            link.addEventListener('click', (e)=> {
-                let newProducts = cartUser[i].products.filter(x => x.idPlato != e.target.id);
-                cartUser[i].products = newProducts;
-                if(cartUser[i].products.length == 0){
-                    let newCartUser = cartUser.filter(u => u.id != cartUser[i].id);
-                    localStorage.setItem('cartProducts', JSON.stringify(newCartUser));
-                } else {
-                    console.log('todavia tiene productos');
-                    localStorage.setItem('cartProducts', JSON.stringify(cartUser));
-                }
-                ((e.target.parentNode).parentNode).remove();
-                window.location.reload();
-            })
         }
+        
+        let date = document.createElement('input');
+        let comensales = document.createElement('input');
+        date.type = 'datetime-local';
+        comensales.type = 'number';
+        comensales.placeholder = 'cantidad de personas';
+        date.name = 'date';
+        comensales.name = 'comensales';
+        div.appendChild(date);
+        div.appendChild(comensales);
         productCartSection.appendChild(div)
         
     }
+    
+    
+    /* link.addEventListener('click', (e)=> {
+        let newProducts = cartUser[i].products.filter(x => x.idPlato != e.target.id);
+        cartUser[i].products = newProducts;
+        if(cartUser[i].products.length == 0){
+            let newCartUser = cartUser.filter(u => u.id != cartUser[i].id);
+            localStorage.setItem('cartProducts', JSON.stringify(newCartUser));
+        } else {
+            localStorage.setItem('cartProducts', JSON.stringify(cartUser));
+        }
+        ((e.target.parentNode).parentNode).remove();
+        window.location.reload();
+    }) */
 
-    let shopCartButton = document.querySelector('#shop-cart');
+    let quantityFields = productCartSection.querySelectorAll('#quantity');
+    for (let r = 0; r < quantityFields.length; r++) {
+        const quantityItem = quantityFields[r];
+        quantityItem.addEventListener('change', (event)=> {
+            let productContainer = event.target.parentNode;
+            let priceProduct = productContainer.querySelector('#precio');
+            let costProduct = productContainer.querySelector('#total');
+            costProduct.value = Number(priceProduct.value) * Number(event.target.value);
+        })
+    }
+
+    let shopCartButton = document.querySelector('.pay-button');
     shopCartButton.addEventListener('click', (e)=> {
         e.preventDefault();
         console.log('estoy enviando el formulario');
